d49bf44fa98bfbb511563817baeb654816eefbc8
neelimaa25 <neelimayadav25@gmail.com>
2021-08-04 21:41:47 +1000

    Part 3: Failing test case
    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,6 +8,8 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -90,4 +92,59 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>Food Order Cost<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    @Test
+    public void not_selecting_any_food_items_from_menu_should_display_order_cost_as_zero() {
+
+        //Act
+        int orderValue = restaurant.displayOrderValue((ArrayList<String>) restaurant.selectedFoodItem);
+
+        //Assert
+        assertEquals(0,orderValue);
+    }
+
+
+    @Test
+    public void selecting_food_items_from_menu_should_display_order_cost_as_438() {
+
+        //Arrange
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sizzling brownie",319);
+
+        restaurant.addToOrder("Sizzling brownie");
+        restaurant.addToOrder("Sweet corn soup");
+
+        //Act
+        int orderValue = restaurant.displayOrderValue((ArrayList<String>) restaurant.selectedFoodItem);
+
+        //Assert
+        assertEquals(438,orderValue);
+    }
+
+
+    @Test
+    public void removing_food_item_from_order_should_display_reduced_cost_as_119() {
+
+        //Arrange
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sizzling brownie",319);
+
+
+        restaurant.addToOrder("Sizzling brownie");
+        restaurant.addToOrder("Sweet corn soup");
+        restaurant.removeFromOrder("Sizzling brownie");
+
+        //Act
+        int orderValue = restaurant.displayOrderValue((ArrayList<String>) restaurant.selectedFoodItem);
+
+        //Assert
+        assertEquals(119,orderValue);
+
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>Food Order Cost<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 }
